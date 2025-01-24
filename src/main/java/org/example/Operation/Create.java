5ed416311e71041968ebad347b250c33183670d2
rashmika <rashmikanethsarani119@gmail.com>
2025-01-25 00:20:22 +0530

    added generate student id part into create method

--- src/main/java/org/example/Operation/Create.java
+++ src/main/java/org/example/Operation/Create.java
@@ -1,23 +1,29 @@
 package org.example.Operation;
 
+import org.example.OTPHandler.GenerateStudentId;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Create {
-    public String addStudents(String fistname, String lastname, String address, int gradeYear, int studentID, String courses, int tuitionBalance, Connection conn) throws SQLException {
-        String sql="INSERT INTO students (first_name,second_name,address,grade_year,student_ID,courses,tuition_balance)VALUES(?,?,?,?,?,?,?)";
+    public String addStudents(String fistname, String lastname, int gradeYear, String courses, int tuitionBalance, Connection conn) throws SQLException {
+
+
+        String sql="INSERT INTO students (first_name,second_name,grade_year,student_ID,courses,tuition_balance)VALUES(?,?,?,?,?,?,?)";
 
         try(PreparedStatement preparedStatement=conn.prepareStatement(sql)){
+
+            int studentID = GenerateStudentId.generateStudentIdAccordingToYear(gradeYear);
+
             preparedStatement.setString(1,fistname);
             preparedStatement.setString(2,lastname);
-            preparedStatement.setString(3,address);
-            preparedStatement.setInt(4,gradeYear);
-            preparedStatement.setInt(5,studentID);
-            preparedStatement.setString(6,courses);
-            preparedStatement.setInt(7,tuitionBalance);
+            preparedStatement.setInt(3,gradeYear);
+            preparedStatement.setInt(4,studentID);
+            preparedStatement.setString(5,courses);
+            preparedStatement.setInt(6,tuitionBalance);
+
             int res=preparedStatement.executeUpdate();
-            preparedStatement.executeUpdate();
 
             if(res>0){
                 return "Successfully added the student record of student id:"+studentID;
