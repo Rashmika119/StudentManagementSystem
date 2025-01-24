979953aac7504ce2f3b475a8ab6da3d5266ebfaa
rashmika <rashmikanethsarani119@gmail.com>
2025-01-24 14:12:36 +0530

    initialize the attributes of the student table

---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Student {
+    //constructor: prompt user to enter students name and the year
+    //generate an id
+    //enroll n courses
+    //View balance
+    //pay tuition
+    //Show status
+
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse=600;
+    private static int id=1001;
+
+}
