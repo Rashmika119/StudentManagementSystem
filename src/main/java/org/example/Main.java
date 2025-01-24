d6ad8116fe4f501f3b94459c005af88c73919db8
rashmika <rashmikanethsarani119@gmail.com>
2025-01-24 14:17:18 +0530

    all other files of the project added

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Ask how many number of students want to add
+        //create n number of new students
+
+
+    }
+}
