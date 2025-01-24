34f9e0c0f2f7e4effa8e6c53cf6dfb38bd2daabd
rashmika <rashmikanethsarani119@gmail.com>
2025-01-24 15:50:27 +0530

    created the update function

--- src/main/java/org/example/Operation/Update.java
+++ src/main/java/org/example/Operation/Update.java
@@ -1,5 +1,29 @@
 package org.example.Operation;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Update {
-    String sql="UPDATE students SET first_name=?,last_name=?,"
+    public String updateEmployee(String StudentID, String attribute,Object value,Connection conn){
+        String sql="UPDATE employee SET"+attribute+"=? WHERE StudentID=?";
+        try(PreparedStatement preparedStatement=conn.prepareStatement(sql)){
+            if(!attribute.equals("firstName") || attribute.equals("lasName") || attribute.equals("courses")){
+
+                preparedStatement.setString(1,value.toString());
+            }else if(attribute.equals("gradeYear") || attribute.equals("tuitionBalance"){
+                preparedStatement.setInt(1,Integer.parseInt(value.toString()));
+            }
+            preparedStatement.setString(2,StudentID);
+            int res=preparedStatement.executeUpdate();
+
+            if(res>0){
+                return "Updated the students record of ID: "+StudentID+" Successfully!";
+            }else{
+                return "Update failed to the student number: "+StudentID;
+            }
+        }catch(SQLException e){
+            return "Can't update the students record!"+e.getMessage();
+        }
+    }
 }
